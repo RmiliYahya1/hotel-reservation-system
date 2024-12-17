fab5c0e1c82d0846acbaff03cbe361baa0b80819
Rmili yahya <rmiliyahya2@gmail.com>
2024-12-17 22:53:29 +0100

    third commit

---
+++ hotel-reservation-desktop-app/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace hotel_reservation_desktop_app;
+
+/// <summary>
+/// Interaction logic for App.xaml
+/// </summary>
+public partial class App : Application
+{
+}
