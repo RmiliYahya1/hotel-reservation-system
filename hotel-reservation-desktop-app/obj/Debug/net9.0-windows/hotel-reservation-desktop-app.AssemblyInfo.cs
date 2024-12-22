676c17bceaa4983d047b1858b3fada88d56a0f08
Radouane ait said <radouane.aitsaid01@gmail.com>
2024-12-22 23:28:55 +0100

    delete web

--- hotel-reservation-desktop-app/obj/Debug/net9.0-windows/hotel-reservation-desktop-app.AssemblyInfo.cs
+++ hotel-reservation-desktop-app/obj/Debug/net9.0-windows/hotel-reservation-desktop-app.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("hotel-reservation-desktop-app")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4fc05dadc1de1dc64830cee7834b221b40a10171")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+06ca8b9f7ebffca3bb46d8715d4eb4970cc936d9")]
 [assembly: System.Reflection.AssemblyProductAttribute("hotel-reservation-desktop-app")]
 [assembly: System.Reflection.AssemblyTitleAttribute("hotel-reservation-desktop-app")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
